0428684a61dc5f4563b76f33cb30dc0da6b67f0e
lad <28843464@qq.com>
2021-01-18 00:11:20 +0800

    合并冲突

--- admin/assets/js/login.js
+++ admin/assets/js/login.js
@@ -2,7 +2,7 @@ let layer = layui.layer,
     form = layui.form;
 
 // 给form表单注册提交事件
-$('form').on('submit', function(e) {
+$('form').on('submit', function (e) {
     // 阻止默认事件
     e.preventDefault();
     // 获取参数
@@ -12,17 +12,14 @@ $('form').on('submit', function(e) {
         type: "post",
         url: "api/login",
         data: params,
-        success: function(res) {
+        success: function (res) {
             layer.msg(res.message);
             console.log(res);
             if (res.status == 0) {
                 location.href = "/admin/index.html";
                 localStorage.setItem('mytoken', res.token);
             } else {
-<<<<<<< HEAD
                 // 表单清空
-=======
->>>>>>> 678f9b2... 文章列表版本
                 $('form')[0].reset();
             }
         }
--- admin/assets/js/swiper/swiper.js
+++ admin/assets/js/swiper/swiper.js
@@ -5,62 +5,29 @@ function loadList() {
     $.ajax({
         type: "get",
         url: "admin/swipers",
-        success: function(res) {
-<<<<<<< HEAD
+        success: function (res) {
             var tags = template('table-tpl', res)
             $('.layui-table tbody').html(tags)
         }
 
-=======
-
-            if (res.status == 0) {
-                let arr = res.data;
-                let str = "";
-                $.each(arr, function(index, item) {
-                    str += `<tr>
-                    <td>${item.id}</td> 
-                    <td>  <img src="http://localhost:8888/uploads/${item.swiperimg}" alt=""></td> 
-                    <td> ${item.swiperimg} </td> 
-                    <td> <span data-id="${item.id}" data-status="${item.swiperstatus}" class="layui-badge layui-bg-green">${item.swiperstatus}</span></td> 
-                    <td>
-                    <button data-id="${item.id}" type="button" class="layui-btn layui-btn-xs layui-btn-danger del">
-                    删除
-                  </button></td>
-                    </tr>`;
-                    if (arr.swiperstatus == 0) {
-                        $('tbody span').addClass('layui-bg-green').removeClass('layui-bg-cyan');
-                        $('tbody span').text('√')
-                    } else {
-                        $('tbody span').addClass('layui-bg-cyan').removeClass('layui-bg-green');
-                        $('tbody span').text('x');
-                    }
-                });
-                $('tbody').html(str);
-            }
-        }
->>>>>>> 678f9b2... 文章列表版本
     });
 }
 loadList();
 var layer = layui.layer;
 
 // 删除功能
-<<<<<<< HEAD
-$('tbody').on('click', '.delete', function() {
-=======
-$('tbody').on('click', '.del', function() {
->>>>>>> 678f9b2... 文章列表版本
+$('tbody').on('click', '.delete', function () {
 
     let id = $(this).attr('data-id');
     layer.confirm('确定要删除吗？', {
         btn: ['YSE', 'NO'],
         icon: 3, //询问信息前显示的图标代码
         title: '提示'
-    }, function(index) {
+    }, function (index) {
         $.ajax({
             type: "delete",
             url: "admin/swipers/" + id,
-            success: function(res) {
+            success: function (res) {
                 layer.msg(res.message);
                 loadList();
             }
@@ -69,22 +36,17 @@ $('tbody').on('click', '.del', function() {
 });
 
 // 修改状态功能
-$('tbody').on('click', 'span', function(e) {
+$('tbody').on('click', 'span', function (e) {
     let data_id = $(this).attr('data-id');
     let status = $(this).attr('data-status');
-<<<<<<< HEAD
     console.log(status);
     $.ajax({
         type: 'put',
         url: "admin/swipers/" + data_id,
-        data: { status, },
-=======
-    $.ajax({
-        type: 'put',
-        url: "admin/swipers/" + data_id,
-        data: status,
->>>>>>> 678f9b2... 文章列表版本
-        success: function(res) {
+        data: {
+            status,
+        },
+        success: function (res) {
             if (res.status == 0) {
                 layer.msg(res.message);
                 loadList();
@@ -94,13 +56,13 @@ $('tbody').on('click', 'span', function(e) {
 });
 
 // 批量上传
-$('#uploadSwiper').on('click', function() {
+$('#uploadSwiper').on('click', function () {
     $('#myfile').click();
 })
-$('body').on('change', '#myfile', function(e) {
+$('body').on('change', '#myfile', function (e) {
     let files = e.target.files
     let fd = new FormData()
-    $.each(files, function(index, item) {
+    $.each(files, function (index, item) {
         fd.append('swipers', item)
     })
     $.ajax({
@@ -110,7 +72,7 @@ $('body').on('change', '#myfile', function(e) {
         // 俩个false
         processData: false,
         contentType: false,
-        success: function(res) {
+        success: function (res) {
             if (res.status === 0) {
                 layer.msg(res.message)
                 loadList()
